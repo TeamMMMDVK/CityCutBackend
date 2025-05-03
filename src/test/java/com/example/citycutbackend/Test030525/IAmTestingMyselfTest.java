ab8b6dab3eef78e0debd196a032384402cb94c4c
DanielJensenKEA <daje0003@stud.kea.dk>
2025-05-04 00:01:37 +0200

    First Commit - More testing of Github Actions

--- src/test/java/com/example/citycutbackend/Test030525/IAmTestingMyselfTest.java
+++ src/test/java/com/example/citycutbackend/Test030525/IAmTestingMyselfTest.java
@@ -3,9 +3,11 @@ package com.example.citycutbackend.Test030525;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class IAmTestingMyselfTest {
     IAmTestingMyself testObject;
 
