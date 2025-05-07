3f4cdc3c77c6c6ecf09e46095f9e3cf20f524aaa
MetteMunch <meha0005@stud.kea.dk>
2025-05-07 14:17:34 +0200

    CITY-129 gemt secretkey i environment credentials

--- src/main/java/com/example/citycutbackend/config/JwtService.java
+++ src/main/java/com/example/citycutbackend/config/JwtService.java
@@ -3,18 +3,19 @@ package com.example.citycutbackend.config;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
-import java.util.Base64;
+import java.security.Key;
 import java.util.Date;
 
 
 @Component
 public class JwtService {
-    private static final String SECRET_KEY = "yR2jX4uUOuvktURD3JGcL8tA8e6M6yvf7m5EGARunCZzP6rL5axHTQ9ZgZXRSZqRW0oqr8AvEysOSAGHghRAAA=="; // Brug en miljøvariabel i produktion
-    private static final SecretKey SECRET = Keys.hmacShaKeyFor(Base64.getDecoder().decode(SECRET_KEY));
+    @Value("${JWT_SECRET_KEY}")
+    private String secretKey;
     private static final long EXPIRATION_TIME = 86400000; // 1 dag i millisekunder
 
     public String generateToken(String email) {
@@ -22,13 +23,13 @@ public class JwtService {
                 .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SECRET, SignatureAlgorithm.HS512)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS512)
                 .compact();
     }
 
     public String extractUsername(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(SECRET)
+                .setSigningKey(getSigningKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody()
@@ -38,13 +39,18 @@ public class JwtService {
     public boolean isTokenValid(String token) {
         try {
             Jwts.parserBuilder()
-                    .setSigningKey(SECRET)
+                    .setSigningKey(getSigningKey())
                     .build()
                     .parseClaimsJws(token);
             return true;
         } catch (JwtException | IllegalArgumentException e) {
             return false;
         }
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
 
