0f4db7019ea03fbf159f13e51fc4e6de9a25f0da
DanielJensenKEA <daje0003@stud.kea.dk>
2025-05-07 10:39:51 +0200

    Endpoint created for list of services available from DB.
    CITY-66 , CITY-67 , CITY-63

--- src/main/java/com/example/citycutbackend/treatments/TreatmentController.java
+++ src/main/java/com/example/citycutbackend/treatments/TreatmentController.java
@@ -1,9 +1,21 @@
 package com.example.citycutbackend.treatments;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@CrossOrigin
+@RequestMapping("api/v1/treatment")
 public class TreatmentController {
+    private final TreatmentService treatmentService;
 
+    public TreatmentController(TreatmentService treatmentService) {
+        this.treatmentService = treatmentService;
+    }
 
+    @GetMapping("/treatments")
+    public List<Treatment> getAllTreatmentsFromDB() {
+        return treatmentService.getAllTreatmentsFromDB();
+    }
 }
