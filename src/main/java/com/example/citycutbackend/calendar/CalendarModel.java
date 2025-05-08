0a22e96e578092e5069276b8de0c757bfa75b6de
KristofferKay <37834268+KristofferKay@users.noreply.github.com>
2025-05-08 11:20:21 +0200

    init

---
+++ src/main/java/com/example/citycutbackend/calendar/CalendarModel.java
@@ -0,0 +1,9 @@
+package com.example.citycutbackend.calendar;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CalendarModel {
+}
---
+++ src/main/java/com/example/citycutbackend/calendar/CalendarRepository.java
@@ -0,0 +1,7 @@
+package com.example.citycutbackend.calendar;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CalendarRepository {
+}
