d127f40ec98a94f44d5dd8afcae02b0f531ea169
MetteMunch <meha0005@stud.kea.dk>
2025-05-11 11:57:45 +0200

    CITY-129 har tilføjet test klasse

---
+++ src/test/java/com/example/citycutbackend/user/UserServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.citycutbackend.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void registerUser() {
+    }
+}
