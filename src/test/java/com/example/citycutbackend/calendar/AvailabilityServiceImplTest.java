8a5487faaeb9ec05cf80c661610d10c9d92c9742
KristofferKay <37834268+KristofferKay@users.noreply.github.com>
2025-05-17 17:45:58 +0200

    added test file.

--- src/test/java/com/example/citycutbackend/calendar/AvailabilityServiceImplTest.java
+++ src/test/java/com/example/citycutbackend/calendar/AvailabilityServiceImplTest.java
@@ -1,10 +1,29 @@
 package com.example.citycutbackend.calendar;
 
+import com.example.citycutbackend.config.JwtAuthFilter;
+import com.example.citycutbackend.config.JwtService;
+import com.example.citycutbackend.user.UserRepository;
+import com.example.citycutbackend.user.UserServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@ActiveProfiles("test")
 class AvailabilityServiceImplTest {
+
+    //For at test kan køre korrekt, må jeg lave Mock af disse bean ifm jwt ellers
+    //vil det ikke compile korrekt. Når test kører så loades hele applikationen nemlig,
+    //også sikkerhed, og disse sikkerheds-beans mangler ....derfor mockes de
+    @MockBean
+    private JwtAuthFilter jwtAuthFilter;
+
+    @MockBean
+    private JwtService jwtService;
 
     @Test
     void getAvailableTimeslotsForDay() {
