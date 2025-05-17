c2e55fba1645f3adc4d4ebf17b3e15a3d44831b3
KristofferKay <37834268+KristofferKay@users.noreply.github.com>
2025-05-17 17:44:02 +0200

    added test file. Test will come later.

---
+++ src/test/java/com/example/citycutbackend/calendar/AvailabilityServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.example.citycutbackend.calendar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AvailabilityServiceImplTest {
+
+    @Test
+    void getAvailableTimeslotsForDay() {
+    }
+
+    @Test
+    void checkAvailabilityForDates() {
+    }
+}
