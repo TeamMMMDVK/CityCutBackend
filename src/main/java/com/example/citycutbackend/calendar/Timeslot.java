5ec195cb63b2c65d2fa9b47ebeac34c4988d204a
GitOnat <maon0001@stud.kea.dk>
2025-05-23 02:01:28 +0200

    backend for adding timeslots

--- src/main/java/com/example/citycutbackend/calendar/Timeslot.java
+++ src/main/java/com/example/citycutbackend/calendar/Timeslot.java
@@ -2,12 +2,12 @@ package com.example.citycutbackend.calendar;
 
 import com.example.citycutbackend.bookings.Booking;
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -20,12 +20,22 @@ public class Timeslot {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private LocalDate date;
     private LocalTime time;
+
+    @Enumerated(EnumType.STRING)
+    private DayOfWeek dayOfWeek;
+
+    @ManyToOne
+    @JoinColumn(name = "stylist_id")
+    private Stylist stylist;
+
     @ManyToOne
     @JoinColumn(name = "booking_id")
     @JsonBackReference("booking-timeslots")
     private Booking booking;
-    boolean isAvailable;
-    int duration;
+
+    private boolean isAvailable;
+    private int duration;
 }
